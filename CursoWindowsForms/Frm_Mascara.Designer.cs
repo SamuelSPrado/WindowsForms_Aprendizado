b9312766cd3c63a9d2c4fc3d3e050342eda31ba5
Samuel Prado <samuel.prado@meep-app.local>
2023-05-08 04:01:22 -0300

    Foram adicionados aos testes um menu principal onde será escolhida as opções de teste. Também icones e imagens

--- CursoWindowsForms/Frm_Mascara.Designer.cs
+++ CursoWindowsForms/Frm_Mascara.Designer.cs
@@ -31,6 +31,7 @@ namespace CursoWindowsForms
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Mascara));
             this.Msk_TextBox = new System.Windows.Forms.MaskedTextBox();
             this.Lbl_Conteudo = new System.Windows.Forms.Label();
             this.Lbl_MascaraAtiva = new System.Windows.Forms.Label();
@@ -155,6 +156,7 @@ namespace CursoWindowsForms
             this.Controls.Add(this.Lbl_MascaraAtiva);
             this.Controls.Add(this.Lbl_Conteudo);
             this.Controls.Add(this.Msk_TextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Frm_Mascara";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Exemplos de Máscaras";
---
+++ CursoWindowsForms/Frm_ValidaCPF2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_ValidaCPF2 : Form
+    {
+        public Frm_ValidaCPF2()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Reset_Click(object sender, EventArgs e)
+        {
+            Msk_CPF.Text = "";
+        }
+
+        private void Btn_Valida_Click(object sender, EventArgs e)
+        {
+            string vConteudo;
+            vConteudo = Msk_CPF.Text;
+            vConteudo = vConteudo.Replace(".", "").Replace("-", "");
+            vConteudo = vConteudo.Trim();
+
+            if(vConteudo == "")
+            {
+                MessageBox.Show("O campo não pode ser vazio",
+                        "Mensagem de Validação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+            }
+            else
+            {
+
+                if (vConteudo.Length != 11)
+                {
+                    MessageBox.Show("O CPF deve conter 11 dígitos!",
+                            "Mensagem de Validação",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Error);
+                }
+
+                else
+                {
+                    if (MessageBox.Show("Você deseja realmente validar o CPF?",
+                "Mensagem de validação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        bool validaCpf = false;
+                        validaCpf = Cls_Uteis.Valida(Msk_CPF.Text);
+                        if (validaCpf)
+                        {
+
+                            MessageBox.Show("CPF Válido!",
+                                "Mensagem de Validação",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("CPF Inválido!",
+                                "Mensagem de Validação",
+                                MessageBoxButtons.OK,
+                                MessageBoxIcon.Error);
+                        }
+                    }
+                }  
+            }
+        }
+    }
+}
